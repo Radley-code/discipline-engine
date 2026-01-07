410757a0f04e362806c5de5ea6433e0a885b9f10
Radley-code <radleyacha00@icloud.com>
2026-01-07 23:26:48 +0100

    removed all secrets

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAey12mGvyWS_4s0D7eCo63sV_xoikDEV8',
-  authDomain: 'discipline-engine-29d21.firebaseapp.com',
-  projectId: 'discipline-engine-29d21',
-  storageBucket: 'discipline-engine-29d21.appspot.com',
-  messagingSenderId: '31220393360',
-  appId: '1:31220393360:android:ed47feb8c3b06c10e9ff15',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
