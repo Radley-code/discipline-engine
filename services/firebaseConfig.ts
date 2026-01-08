f99cbf164506ce0efb28bcf2fd29e12e4edb5879
Radley-code <radleyacha00@icloud.com>
2026-01-09 00:02:13 +0100

    remove .env add secret to app.json

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,14 +1,15 @@
+import Constants from 'expo-constants';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: Constants.expoConfig?.extra?.firebase?.apiKey,
+  authDomain: Constants.expoConfig?.extra?.firebase?.authDomain,
+  projectId: Constants.expoConfig?.extra?.firebase?.projectId,
+  storageBucket: Constants.expoConfig?.extra?.firebase?.storageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebase?.messagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebase?.appId,
 };
 
 const app = initializeApp(firebaseConfig);
